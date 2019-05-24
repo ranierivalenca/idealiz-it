cf2e29004283be15b44119263dc977f119402e0e
Ranieri <ranierivalenca@gmail.com>
2019-05-24 11:58:19 -0300

    Exibindo ideias do usuário na sua home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $ideas = \App\Idea::where('user_id', Auth::user()->id)->get();
+        return view('home', compact(['ideas']));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,20 @@
                 </div>
             </a>
         </div>
+        @foreach ($ideas as $idea)
+            <div class="col-md-4" style="margin-bottom: 1em">
+                <a href="{{ route('ideas.show', $idea->id) }}">
+                    <div class="card">
+                        <div class="card-header">
+                            {{ $idea->title }}
+                        </div>
+                        <div class="card-body">
+                            {!! nl2br($idea->description) !!}
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
         <!-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('messages.home.dashboard', ['name' => Auth::user()->name]) }}</div>
